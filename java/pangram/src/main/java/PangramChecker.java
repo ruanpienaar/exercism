c190898cb135905a3842f14e27068ec708fc07d5
ruanpienaar <ruan800@gmail.com>
2018-05-18 16:58:51 +0100

    Java - Pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,36 @@
+/**
+ * A pangram (Greek: παν γράμμα, pan gramma, "every letter") is a sentence using every letter of the alphabet at least once.
+ * The best known English pangram is:
+ * The quick brown fox jumps over the lazy dog.
+ * @author ruanpienaar
+ * @version 0.1
+ */
+
 public class PangramChecker {
 
+    /**
+     *
+     * @param input String
+     * @return boolean
+     * 97 = a, 122 = z
+     */
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // if input smaller than the alphabet ?
+        if(input.length() < 26) {
+            return false;
+        } else {
+            String pangranTestStr = "";
+            for (int i = 0; i < input.length(); i++) {
+                char lowerCaseChar = String.valueOf(input.charAt(i)).toLowerCase().charAt(0);
+                int charIntVal = (int) lowerCaseChar;
+                if(charIntVal >= 97 && charIntVal <= 122){ // if a-z
+                    if(! pangranTestStr.contains(String.valueOf(lowerCaseChar))){ // if known
+                        pangranTestStr = pangranTestStr.concat(String.valueOf(lowerCaseChar));
+                    }
+                }
+            }
+            return pangranTestStr.length() == 26;
+        }
     }
 
 }
