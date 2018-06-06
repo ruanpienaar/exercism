b85456d9637082ef6a1c8af83284cb186eb6efba
ruanpienaar <ruan800@gmail.com>
2018-06-06 11:26:25 +0100

    ..

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -1,32 +1,51 @@
 defmodule NucleotideCount do
-  @nucleotides [?A, ?C, ?G, ?T]
+    @nucleotides [?A, ?C, ?G, ?T]
 
-  @doc """
-  Counts individual nucleotides in a NucleotideCount strand.
+    @doc """
+    Counts individual nucleotides in a NucleotideCount strand.
 
-  ## Examples
+    ## Examples
 
-  iex> NucleotideCount.count('AATAA', ?A)
-  4
+    iex> NucleotideCount.count('AATAA', ?A)
+    4
 
-  iex> NucleotideCount.count('AATAA', ?T)
-  1
-  """
-  @spec count([char], char) :: non_neg_integer
-  def count(strand, nucleotide) do
+    iex> NucleotideCount.count('AATAA', ?T)
+    1
+    """
+    @spec count([char], char) :: non_neg_integer
 
-  end
+    def count([], nucleotide) do
+        0
+    end
+    def count(strand, nucleotide) do
+        Enum.reduce(strand, 0,
+            fn(i, acc) ->
+                case i == nucleotide do
+                    true ->
+                        acc+1
+                    false ->
+                        acc
+                end
+            end
+        )
+    end
 
-  @doc """
-  Returns a summary of counts by nucleotide.
+    @doc """
+    Returns a summary of counts by nucleotide.
 
-  ## Examples
+    ## Examples
 
-  iex> NucleotideCount.histogram('AATAA')
-  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
-  """
-  @spec histogram([char]) :: map
-  def histogram(strand) do
+    iex> NucleotideCount.histogram('AATAA')
+    %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+    """
+    @spec histogram([char]) :: map
+    def histogram(strand) do
+        %{
+            ?A => count(strand, ?A),
+            ?T => count(strand, ?T),
+            ?C => count(strand, ?C),
+            ?G => count(strand, ?G)
+        }
 
-  end
+    end
 end
