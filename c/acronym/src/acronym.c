6df87ae756904229f8c5190383f4f821dcf3afa4
ruanpienaar <ruan800@gmail.com>
2018-07-01 13:51:34 +0100

    Cleaner Version

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -4,20 +4,18 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-char *concat(const char *s1, const char *s2){
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the null-terminator
-    //in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
+char *concat(char *s1, const char *letter){
+    int size = strlen(s1)+1+1; //+1=letter, +1=null-terminator
+    char *result = strncat(s1, letter, size);
     return result;
 }
 
-char *chartoupper(char *upperstr, char letter){
+char *chartoupper(char letter){
     unsigned char cu;
     cu = toupper(letter);
-    upperstr[0] = cu;
-    upperstr[1] = '\0';
-    return upperstr;
+    char *u = malloc(2);
+    sprintf(u, "%c", cu);
+    return u;
 }
 
 char *abbreviate(const char *phrase){
@@ -25,26 +23,15 @@ char *abbreviate(const char *phrase){
         return NULL;
     } else {
         int len = strlen(phrase);
-        // 1 - take the first letter & uppercase it
-        // abbreviation = string that returns as the abbreviation.
-        char firstletter[2];
-        char * abbreviation = chartoupper(firstletter, phrase[0]);
-        // start with 0 since we already took first letter, 
-        // wait for space/punctuation to set to 1.
+        char *abbreviation = chartoupper(phrase[0]);
         int take_letter_flag = 0;
-        // loop over each letter in phrase
         for(int i = 0; i < len; i++){
             char charletter = phrase[i];
-            // when at the first letter, and not a space/punctuation
             if(take_letter_flag && (charletter != ' ' || charletter != '-')){
-                // 1 - create String from char letter, and uppercase letter
-                char strupperletter[2];
-                char *strletter = chartoupper(strupperletter, charletter);
-                // 2 - concat letter to Abbreviation string
+                char *strletter = chartoupper(charletter);
                 abbreviation = concat(abbreviation, strletter);
                 take_letter_flag = 0; // unset flag
             }
-            // when space/puncuation encoutered
             if(charletter == ' ' || charletter == '-'){
                 take_letter_flag = 1;
             }
