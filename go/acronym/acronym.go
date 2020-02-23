ef355dd99db47ddcdd00e7f4907136e41ac61833
ruanpienaar <ruan800@gmail.com>
2020-02-23 13:06:01 +0000

    more go practice

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,26 +1,29 @@
+// acronym takes in a string and creates a acronym by taking the first letter of each word
+// and appending that to create the acronym
 package acronym
-import(
-    //"fmt"
+
+import (
+    "regexp"
     "strings"
 )
 
-const testVersion = 2
+// bare minimal to allow test passing
+// first time challenge with string,byte,slices
 
+// Abbreviate returns a Abbreviated string from string s
 func Abbreviate(s string) string {
-    var replacer = strings.NewReplacer(
-        ":", " ",
-        "-", " ")
-    s = replacer.Replace(s)
-    var tokens []string = strings.Split(s, " ")
-    var acronym string
-    for i := 0; i<len(tokens); i++ {
-        var t string = tokens[i]
-        if(t != ""){
-
-            //HyperText
-
-            acronym += strings.ToUpper(string(t[0]))
-        }
-    }
-    return acronym
+	accronym := []byte{}
+	word_seperators := []byte{' ', '-'}
+	first_letter_taken := false
+	var alphaRe = regexp.MustCompile(`[a-zA-Z]`)
+	for i := 0; i < len(s); i++ {
+	    if(!first_letter_taken && alphaRe.MatchString(string(s[i]))) {
+	        first_letter_taken = true
+	        accronym = append(accronym, strings.ToUpper(string(s[i]))[0])
+	    } else if (first_letter_taken && ( s[i] == word_seperators[0] || s[i] == word_seperators[1] )) {
+	    	// potentiall loop over word_seperators ?
+	        first_letter_taken = false
+	    }
+	}
+	return string(accronym)
 }
